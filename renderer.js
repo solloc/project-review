72b04bbbb0801ec355fa4f8de24f95c103ab1268
Matthias Maisch <matthias@maisch.me>
2021-05-24 16:50:25 +0200

    showing notification in the renderer process

---
+++ renderer.js
@@ -0,0 +1,7 @@
+const myNotification = new Notification('Title', {
+    body: 'Notification from Renderer process'
+})
+
+myNotification.onclick = () => {
+    console.log('Notification clicked')
+}
